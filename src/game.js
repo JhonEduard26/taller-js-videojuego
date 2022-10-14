7ed842e64d574e208f9217780b3dc378abc45ced
Jhon Eduard Bocanegra <jhon.bocanegra26@gmail.com>
2022-10-14 10:05:46 -0500

    [ADD] canvas responsive

--- src/game.js
+++ src/game.js
@@ -1,30 +1,33 @@
 const canvas = document.querySelector('#game')
 const game = canvas.getContext('2d')
 
+window.addEventListener('load', setCanvasSize)
+window.addEventListener('resize', setCanvasSize)
 
-window.addEventListener('load', startGame)
-window.addEventListener('resize', setSize)
+let canvasSize = 0
+let elementsSize = 0
 
-function setSize() {
-  let canvasSize
-
+function setCanvasSize() {
   window.innerWidth > window.innerHeight
-    ? canvasSize = window.innerHeight * 0.65
-    : canvasSize = window.innerWidth * 0.75
+    ? canvasSize = window.innerHeight * 0.6
+    : canvasSize = window.innerWidth * 0.6
 
   canvas.setAttribute('width', canvasSize)
   canvas.setAttribute('height', canvasSize)
 
-  const elementsSize = (canvasSize / 10)
+  elementsSize = canvasSize / 10
+  startGame()
+}
 
+function renderMap() {
   game.font = elementsSize + 'px sans-serif'
-  game.textAlign = 'center'
+  game.textAlign = 'end'
 
   for (let i = 1; i <= 10; i++) {
-    game.fillText(emojis['X'], elementsSize, elementsSize * i)
+    game.fillText(emojis['X'], elementsSize + 5, elementsSize * i)
   }
 }
 
 function startGame() {
-  setSize()
+  renderMap()
 }
